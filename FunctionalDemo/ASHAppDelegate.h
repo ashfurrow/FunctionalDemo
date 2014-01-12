1a3cf0282c16a7f381f49383f58e97824a1cd18f
Ash Furrow <ash@ashfurrow.com>
2014-01-12 09:55:57 -0500

    Initial Commit

---
+++ FunctionalDemo/ASHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASHAppDelegate.h
+//  FunctionalDemo
+//
+//  Created by Ash Furrow on 1/12/2014.
+//  Copyright (c) 2014 Ash Furrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FunctionalDemo/ASHViewController.h
@@ -0,0 +1,13 @@
+//
+//  ASHViewController.h
+//  FunctionalDemo
+//
+//  Created by Ash Furrow on 1/12/2014.
+//  Copyright (c) 2014 Ash Furrow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASHViewController : UIViewController
+
+@end
