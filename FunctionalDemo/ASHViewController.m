07d8647baa487a3d854c6d8a1d105ec12a1a482a
Ash Furrow <ash@ashfurrow.com>
2014-01-12 12:31:53 -0500

    Finished.

--- FunctionalDemo/ASHViewController.m
+++ FunctionalDemo/ASHViewController.m
@@ -26,6 +26,7 @@
                           [[ASHPerson alloc] initWithName:@"Bob" favouriteColor:[UIColor blueColor] age:40],
                           [[ASHPerson alloc] initWithName:@"Carol" favouriteColor:[UIColor purpleColor] age:18]];
     
+    // Log the names as an array, log the max age, and set the background colour to that person's favourite colour.
     [self nonFunctionalMethod];
 //    [self functionalMethod];
 }
@@ -37,14 +38,42 @@
 
 
 
+
+
+
+
+
 -(void)nonFunctionalMethod {
-    /*
-     Note: I'm cheating here a bit because there is a non-RXCollections way to gather names using valueForKey:
-     But that's a functional way to do it, so ¯\_(ツ)_/¯
-     */
+    // First create the names array to log
+    NSMutableArray *namesMutableArray = [NSMutableArray arrayWithCapacity:self.personsArray.count];
+    
+    for (ASHPerson *person in self.personsArray) {
+        [namesMutableArray addObject:person.name];
+    }
+    
+    NSArray *namesArray = [NSArray arrayWithArray:namesMutableArray];
+    NSLog(@"%@", namesArray);
     
     
+    // Next find the maximum age and that person's favourite colour
+    NSInteger maximumAgeSoFar = NSIntegerMin;
+    UIColor *favouriteColourOfMaximumAgeSoFar;
+    
+    for (ASHPerson *person in self.personsArray) {
+        if (person.age > maximumAgeSoFar) {
+            maximumAgeSoFar = person.age;
+            favouriteColourOfMaximumAgeSoFar = person.favouriteColour;
+        }
+    }
+    
+    NSLog(@"Maximum age is %d", maximumAgeSoFar);
+    self.view.backgroundColor = favouriteColourOfMaximumAgeSoFar;
 }
+
+
+
+
+
 
 
 
@@ -53,8 +82,28 @@
 
 
 -(void)functionalMethod {
+    NSArray *namesArray = [self.personsArray rx_mapWithBlock:^id(ASHPerson *each) {
+        return each.name;
+    }];
     
+    NSLog(@"%@", namesArray);
+    
+    ASHPerson *oldestPerson = [self.personsArray rx_foldWithBlock:^id(ASHPerson *memo, ASHPerson *each) {
+        if (memo) {
+            return memo.age > each.age ? memo : each;
+        } else {
+            return each;
+        }
+    }];
+    
+    NSLog(@"Maximum age is %d", oldestPerson.age);
+    self.view.backgroundColor = oldestPerson.favouriteColour;
 }
+
+
+
+
+
 
 
 
